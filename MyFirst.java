da6cadc52db6b85bb7e246f91b45ea3a2a7839fb
Harsha Gudise <sreeharshadbz95@gmail.com>
2023-02-25 17:59:00 +0530

    First Program in java

---
+++ MyFirst.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class MyFirst
+{
+    public static void main(String[] args) {
+        System.out.println("harsha");
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println(a);
+
+    }
+}
